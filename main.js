7e0e594bfa15a9ad82a6e650636481b4fe6183c0
wiktortr <wiktortr9@gmail.com>
2018-06-15 15:29:47 +0200

    Fix CORS

--- main.js
+++ main.js
@@ -3,6 +3,12 @@ const fs = require("fs");
 
 let app = express();
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.get("/", function(req, res){
     res.send("VChessServer");
 });
