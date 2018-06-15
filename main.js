d86b6ba85805c2757f6fa8ea7e763c59ae240799
wiktortr <wiktortr9@gmail.com>
2018-06-15 20:37:31 +0200

    Update

--- main.js
+++ main.js
@@ -1,29 +1,37 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 const fs = require("fs");
 const crypto = require("crypto");
 
 let app = express();
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ type: 'application/*+json' }));
+app.use(cors());
+
+app.use(function(req, res, next){
+    console.log(Date.now() + ":Request " + req.ip + " " + req.method + " " + req.url)
     next();
-});
+})
 
 app.get("/", function(req, res){
     res.send("VChessServer");
 });
 
 app.post("/upload", function(req, res){
-    if(req.param("type") == "game" && req.param("data") !== undefined){
-        let data = req.param("data");
+    let reqData = req.body;
+    if(reqData.type == "game" && reqData.data !== undefined){
+        let data = reqData.data;
         let name = crypto.createHash('md5').update(data).digest("hex");
         fs.writeFileSync("games/"+name+".json", data);
+        console.log("Register Game: " + name);
+        res.send("OK");
     }
     else{
         res.send("Error");
     }
-    res.send("OK");
+    res.end();
 });
 
 app.listen(3000);
