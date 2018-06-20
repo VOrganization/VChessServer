b11e86b786fdfd5b8dd6bb404a0c326398cba315
wiktortr <wiktortr9@gmail.com>
2018-06-20 18:18:52 +0200

    Update

--- main.js
+++ main.js
@@ -5,6 +5,40 @@ const mysql = require("mysql2");
 const fs = require("fs");
 const crypto = require("crypto");
 
+const chessboard = [
+    [-1, -2, -3, -4, -5, -3, -2, -1],
+    [-6, -6, -6, -6, -6, -6, -6, -6],
+    [ 0,  0,  0,  0,  0,  0,  0,  0],
+    [ 0,  0,  0,  0,  0,  0,  0,  0],
+    [ 0,  0,  0,  0,  0,  0,  0,  0],
+    [ 0,  0,  0,  0,  0,  0,  0,  0],
+    [ 6,  6,  6,  6,  6,  6,  6,  6],
+    [ 1,  2,  3,  4,  5,  3,  2,  1]
+];
+
+function CloneChessBoard(chessboard){
+    let newBoard = new Array();
+    for (let y = 0; y < chessboard.length; y++) {
+        let arr = new Array();
+        for (let x = 0; x < chessboard[y].length; x++) {
+            arr.push(chessboard[y][x]);
+        }
+        newBoard.push(arr);
+    }
+    return newBoard;
+}
+
+function CalcInputChessBoard(chessboard, side){
+    let input = new Array();
+    input.push(side);
+    for (let y = 0; y < chessboard.length; y++) {
+        for (let x = 0; x < chessboard[y].length; x++) {
+            input.push(chessboard[y][x]);
+        }
+    }
+    return input;
+}
+
 let connection = mysql.createConnection({
     host: "localhost",
     user: "chess",
@@ -63,21 +97,76 @@ app.post("/upload", (req, res) =>{
 
     let reqData = req.body;
     if(reqData.type == "game" && reqData.data !== undefined){
-        let data = reqData.data;
+        let data = JSON.parse(String(reqData.data).replace("\\", ""));
 
         let players = data.players;
+        let SPlayers = JSON.stringify(players);
         let moves = data.moves;
-        let hash = crypto.createHash('md5').update(moves).digest("hex");
+        let SMoves = JSON.stringify(moves);
+        let hash = crypto.createHash('md5').update(JSON.stringify(moves)).digest("hex");
 
-        // calc traning data
+        console.log(JSON.stringify(players));
+        for (let i = 0; i < players.length; i++) {
+            let p = players[i];
+            p["quality"] = 0;
+            p.points = 0;
+            if(p.side == "black"){
+                p["sideID"] = 0;
+            }
+            else{
+                p["sideID"] = 1;
+            }
+            if(p.won){
+                p.quality = 100;
+            }
+        }
 
-        Query('INSERT INTO `games` (`ID`, `Hash`, `Players`, `Moves`, `Date`) VALUES(?, ?, ?, ?, ?)',
-        [null, '','','','']).then(e => {
+        let points = 0;
+        let prevBoard0 = CloneChessBoard(chessboard);
+        let prevBoard1 = CloneChessBoard(chessboard);
+        let trainData = [];
+        for (let i = 0; i < moves.length; i++) {
+            console.log(JSON.stringify(moves[i]));
+            let out = {
+                input: CalcInputChessBoard(prevBoard0, players[moves[i].player].sideID),
+                output: [ moves[i].x1, moves[i].y1, moves[i].x2, moves[i].y2 ],
+                quality: players[moves[i].player].quality,
+                hash: ""
+            }
+            out.hash = crypto.createHash('md5').update(JSON.stringify(out.input) + JSON.stringify(out.output)).digest("hex");
+
+            let enemy = Math.abs(prevBoard0[moves[i].y2][moves[i].x2]);
+            prevBoard0[moves[i].y2][moves[i].x2] = prevBoard0[moves[i].y1][moves[i].x1];
+            prevBoard0[moves[i].y1][moves[i].x1] = 0;
+
+            switch (enemy) {
+                case 6:{
+                    players[moves[i].player].points += 1;
+                    points += 1;
+                    break;
+                }
+            
+                default:
+                    break;
+            }
+
+
+            console.log(out.hash + "  Q: " + out.quality);
+            trainData.push(out);
+        }
+
+        fs.writeFileSync("train.json", JSON.stringify(trainData));
+
+        Query("INSERT INTO `games` (`ID`, `Hash`, `Players`, `Moves`, `Date`) VALUES(NULL, '"+hash+"', '"+SPlayers+"', '"+SMoves+"', NOW())").then(e => {
+            //calc train data;
+
+
+
             res.json({
                 status: 0,
                 msg: "Success",
                 response: "Success",
-            })
+            });
             res.end();
         }).catch(e => {
             res.json(e);
@@ -86,6 +175,7 @@ app.post("/upload", (req, res) =>{
 
     }
     else{
+        console.log("Error");
         res.json({
             status: -1,
             msg: "Error: invalid request",
